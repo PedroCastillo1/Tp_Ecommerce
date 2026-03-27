376d6038a8cf5471a8916b7070fec501876c0a78
Luquita-DG <lucadigresia01@gmail.com>
2026-03-27 17:19:41 -0300

    mierda xd

--- src/main/java/com/uade/tpo/ecommerce/model/User.java
+++ src/main/java/com/uade/tpo/ecommerce/model/User.java
@@ -3,7 +3,6 @@ package com.uade.tpo.ecommerce.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,7 +17,5 @@ public class User {
     @Column(unique = true)
     private String password;
 
-    @ManyToMany(mappedBy = "users")
-    private java.util.Set<Product> products;
 
 }
--- src/main/java/com/uade/tpo/ecommerce/repository/ICategoriaRepository.java
+++ src/main/java/com/uade/tpo/ecommerce/repository/ICategoriaRepository.java
@@ -1,6 +1,6 @@
 package com.uade.tpo.ecommerce.repository;
 
-import java.util.Locale;
+//import java.util.Locale;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
